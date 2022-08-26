3267eb27cb280f55f09765a2145359db13479753
Yurand <yurand.coloma@tecsup.edu.pe>
2022-08-26 12:39:45 -0500

    only accept a md files when you upload

--- src/App.tsx
+++ src/App.tsx
@@ -156,7 +156,13 @@ function App() {
               >
                 drag your file here
               </div> */}
-              <input ref={uploadRef} className="md_editor-content_body-uploadFile"  type="file" onChange={handleUploadFile} />
+              <input
+                ref={uploadRef}
+                className="md_editor-content_body-uploadFile"
+                type="file"
+                accept=".md"
+                onChange={handleUploadFile}
+              />
               <button className="md_editor-content_body-copyButton" onClick={handleCopy}>
                 <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 448 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg" data-darkreader-inline-fill="" data-darkreader-inline-stroke="">
                   <path d="M320 448v40c0 13.255-10.745 24-24 24H24c-13.255 0-24-10.745-24-24V120c0-13.255 10.745-24 24-24h72v296c0 30.879 25.121 56 56 56h168zm0-344V0H152c-13.255 0-24 10.745-24 24v368c0 13.255 10.745 24 24 24h272c13.255 0 24-10.745 24-24V128H344c-13.2 0-24-10.8-24-24zm120.971-31.029L375.029 7.029A24 24 0 0 0 358.059 0H352v96h96v-6.059a24 24 0 0 0-7.029-16.97z"></path>
